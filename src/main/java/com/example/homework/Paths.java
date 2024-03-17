8f00ed78ecff3563b49d5a9368449b29f7a9e58b
Jiří Loukota <jloukota@seznam.cz>
2024-03-17 09:19:54 +0100

    Created class Paths which holds paths to files with articles.

---
+++ src/main/java/com/example/homework/Paths.java
@@ -0,0 +1,5 @@
+package com.example.homework;
+
+public class Paths {
+    public static final String sciFi = "Articles/sci-fi.txt", romantic = "Articles/romantic.txt", historic = "Articles/historic.txt";
+}
